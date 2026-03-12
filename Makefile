1393ab7abc45e423bef6380a4c98a7a855418a6d
Tomáš Diblík <dibla.tomas@post.cz>
2026-03-12 13:29:25 +0100

    [Feature] Makefile and HttpRequest struct

---
+++ Makefile
@@ -0,0 +1,24 @@
+CC = clang
+COMMON_FLAGS = -std=c99 -Wall -Wextra -Wshadow -Wpedantic -Wconversion -Wformat=2 -fstack-protector-strong -Werror
+
+SRC = server.c
+BIN_DIR = bin
+TARGET = $(BIN_DIR)/server
+
+all: dev run
+
+dev: $(SRC)
+	@mkdir -p $(BIN_DIR)
+	$(CC) $(COMMON_FLAGS) -O0 $(SRC) -o $(TARGET)
+
+prod: $(SRC)
+	@mkdir -p $(BIN_DIR)
+	$(CC) $(COMMON_FLAGS) -O3 $(SRC) -o $(TARGET)
+
+run:
+	./$(TARGET)
+
+clean:
+	rm -rf $(BIN_DIR)
+
+.PHONY: all dev prod run clean
--- server.c
+++ server.c
@@ -4,12 +4,25 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
 
 #define PORT 80
+#define LOG_IP true
+#define MAX_HEADERS_LEN 8192 // same as apache tomcat 6 (https://www.geekersdigest.com/max-http-request-header-size-server-comparison/)
 
+// note on usage: we can safely convert sockaddr_in into sockaddr, since they are 1:1 aligned in memory
 typedef struct sockaddr sockaddr;
 typedef struct sockaddr_in sockaddr_in;
-void handle_client(int client_soc, sockaddr_in* client_adress, socklen_t* client_adress_len);
+typedef struct {
+    char method[16];
+    char path[256];
+    char protocol[16];
+    char *body;
+} HttpRequest;
+
+void handle_client(int client_soc);
+int read_protocol_info(int client_soc, HttpRequest* req);
 
 int main(void) {
   int server_soc = socket(AF_INET, SOCK_STREAM, 0);
@@ -50,7 +63,15 @@ int main(void) {
       perror("client_soc accept failed");
       continue;
     }
-    handle_client(client_soc, &client_adress, &client_adress_len);
+
+    if (LOG_IP) {
+      char ip_str[INET_ADDRSTRLEN];
+      if (inet_ntop(AF_INET, &client_adress.sin_addr, ip_str, INET_ADDRSTRLEN) != NULL) {
+        printf("client_soc accepted connection from %s\n", ip_str);
+      }
+    }
+
+    handle_client(client_soc);
     close(client_soc);
   }
 
@@ -58,6 +79,38 @@ int main(void) {
   return 0;
 }
 
-void handle_client(int client_soc, sockaddr_in* client_adress, socklen_t* client_adress_len) {
-    // todo: implemnet, too tired to do it now.
+void handle_client(int client_soc) {
+  HttpRequest req = {0};
+  switch (read_protocol_info(client_soc, &req)) {
+    case -1:
+      perror("client didn't send protocol info bytes. closing.");
+      return;
+  };
+
+  printf("%s %s: %s\n", req.protocol, req.method, req.path);
 }
+
+int read_protocol_info(int client_soc, HttpRequest* req) {
+  size_t protocol_info_buf_len = sizeof(req->method) + sizeof(req->path) + sizeof(req->protocol);
+  char protocol_info_buf[protocol_info_buf_len];
+  ssize_t bytes_read = read(client_soc, &protocol_info_buf, protocol_info_buf_len-1);
+  if (bytes_read < 0) {
+    return -1;
+  }
+  protocol_info_buf[bytes_read] = '\0';
+  sscanf(protocol_info_buf, "%15s %255s %15s", req->method, req->path, req->protocol);
+  return 0;
+}
+
+// void read_headers(int client_soc) {
+//   // int total_bytes = 0;
+//   // int n;
+//   // while (total_bytes < MAX_HEADERS_LEN) {
+//   //   n = read(client_soc, )
+//   // }
+//   // todo: implement
+// }
+
+// void read_body() {
+//   // todo: implement
+// }
